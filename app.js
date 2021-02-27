49e18457965aee71f163c088022b120e62b683bd
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-02-27 17:40:43 +0200

    Set useFindAndModify to false

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { useFindAndModify: false });
 
 connect.then((db) => {
   console.log("Connected correctly to server");
