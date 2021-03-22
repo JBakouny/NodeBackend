5a7aabc1703d014ca22ccd5ba95feecde223fecd
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-03-22 23:42:43 +0200

    File Upload

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var config = require('./config')
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -55,6 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
